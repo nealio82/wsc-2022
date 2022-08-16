f27bf58dc7c0220d9174feae0f91941aa6793c9d
Neal Brooks <neali82@gmail.com>
2022-08-16 15:22:16 +0100

    add easyadmin and permissive user provider

---
+++ app/src/Security/User.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class User implements UserInterface
+{
+    private string $email;
+
+    private array $roles = [];
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /** @see UserInterface */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return \array_unique($roles);
+    }
+
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+
+    /** @see UserInterface */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
