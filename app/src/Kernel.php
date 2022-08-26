3e9562ed88efb6d1d95fa731ef1fb1a6548db781
Neal Brooks <neali82@gmail.com>
2022-08-26 14:15:24 +0100

    write to /tmp when on lambda

--- app/src/Kernel.php
+++ app/src/Kernel.php
@@ -8,4 +8,27 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function isLambda(): bool
+    {
+        return getenv('LAMBDA_TASK_ROOT') !== false;
+    }
+
+    public function getCacheDir(): string
+    {
+        if ($this->isLambda()) {
+            return '/tmp/cache/' . $this->environment;
+        }
+
+        return parent::getCacheDir();
+    }
+
+    public function getLogDir(): string
+    {
+        if ($this->isLambda()) {
+            return '/tmp/log/';
+        }
+
+        return parent::getLogDir();
+    }
 }
